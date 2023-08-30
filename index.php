71a84913daccd14cec2da59b616723cee03aaa9b
Shubham Chaudhary <100894298+shubhamchaudhary18@users.noreply.github.com>
2023-08-30 10:43:58 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,319 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <meta property="og:title" content="Mandna" />
+    <meta property="og:description" content="Making Healthcare Readily Available" />
+    <meta property="og:image" content="https://Mandna.netlify.app/images/Mandna-logos_transparent.png" />
+    <meta property="og:image:secure_url" content="https://Mandna.netlify.app/images/logo-white.png" />
+    <meta property="og:url" content="https://Mandna.netlify.app" />
+    <meta property="og:type" content="website" />
+    <meta property="og:image:type" content="images/Mandna-logos_transparent.png" />
+    <meta property="og:image:width" content="400" />
+    <meta property="og:image:height" content="300" />
+    <meta property="og:image:alt" content="Mandna" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Hind:wght@300;400;500&display=swap" rel="stylesheet">
+    <link rel="shortcut icon" href="images/Mandna-logos_transparent.png">
+    <link rel="apple-touch-icon" href="apple-touch-icon.png">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Hind+Madurai:wght@300;400;500&display=swap" rel="stylesheet">
+    <title>Mandna - Making Healthcare Readily Accessible</title>
+
+    <link rel="stylesheet" href="css/styleindex.css">
+</head>
+
+<body>
+    <header>
+        <div class="logo">
+            <a href="#"><img src="images/Mandna-logos_transparent.png" alt="Mandna"></a>
+        </div>
+        <div class="toggle">
+            <center>
+                <div class="ham-img">
+                    <img src="images/Mandna-logos_white.png" alt="Mandna">
+                </div>
+            </center>
+            <nav id="hamDIV" class="hnav-links" style="display: block;">
+                <center>
+                    <ul>
+                      
+                        <li class="btn hactive">
+                            <a href="./findhospital.html">Find a Hospital</a>
+                        </li>
+                        <li class="btn">
+                            <a href="applicationlayer/Doctorpatient.php">Consult Now</a>
+                        </li>
+                       
+                        <li class="btn">
+                            <a href="contactus.html">Customer Support</a>
+                        </li>
+                        <li class="btn">
+                            <a href="team.html">Team</a>
+                        </li>
+                        <div class="btn-toolbar mb-2 mb-md-0">
+            <div class="btn d-flex">
+                <a class="btn btn-outline-light mr-2 btns" href="applicationlayer/login.php">Login</a>
+                
+            </div>
+        </div>
+                    </ul>
+                </center>
+            </nav>
+        </div>
+        <a href="#" class="navicon" id="ico" onclick="ham()"></a>
+        <div id="myDIV" class="nav-links">
+            <ul>
+                
+                <li class="btn hactive">
+                    <a href="./findhospital.html">Find a Hospital</a>
+                </li>
+                <li class="btn">
+                <a href="applicationlayer/Doctorpatient.php">Consult Now </a>
+                </li>
+                
+                <li class="btn">
+                    <a href="contactus.html">Customer Support</a>
+                </li>
+                <li class="btn">
+                    <a href="team.html">Team</a>
+                </li>
+                <div class="btn-toolbar mb-2 mb-md-0">
+                    <div class="btn d-flex">
+                        <a class="btn btn-outline-light mr-2 btns" href="applicationlayer/login.php">Login</a>
+                        
+                    </div>
+                </div>
+            </ul>
+        </div>
+    </header>
+    <section class="introduction">
+        <div class="intro-flex" style="display: none;">
+            <img src="images/intro-img.png" alt="About Us">
+            <div class="intro-detail">
+                <h2 class="disappear">About Us</h2>
+                <h1>Health Ki Tension?</h1>
+                <p>
+                   तत्काल चिकित्सक परामर्श करें
+                </p>
+                <p>
+                   #SochMatPoochLe
+                </p>
+               
+            </div>
+        </div>
+        <div class="intro-slides">
+
+            <div class="slideshow-container">
+                <div class="mySlides fade">
+                    <img src="images/slide4.jpg" style="width:100%">
+                    <div class="text">
+                    <h1>Health Ki Tension?</h1>
+                <p>
+                   तत्काल चिकित्सक परामर्श करें
+                </p>
+                <p>
+                   #SochMatPoochLe
+                </p>
+                        <a href="applicationlayer/Doctorpatient.php"><button>Consult Now</button></a>
+                    </div>
+                </div>
+
+                <div class="mySlides fade">
+                    <img src="images/slide2-img.jpg" style="width:100%">
+                    <div class="text">
+                    <h1>Health Ki Tension?</h1>
+                <p>
+                   तत्काल चिकित्सक परामर्श करें
+                </p>
+                <p>
+                   #SochMatPoochLe
+                </p>
+                        <a href="applicationlayer/Doctorpatient.php"><button>Consult Now</button></a>
+                    </div>
+                </div>
+
+                <div class="mySlides fade">
+                    <img src="images/slide3-img.jpg" style="width:100%">
+                    <div class="text">
+                    <h1>Health Ki Tension?</h1>
+                <p>
+                   तत्काल चिकित्सक परामर्श करें
+                </p>
+                <p>
+                   #SochMatPoochLe
+                </p>
+                        <a href="applicationlayer/Doctorpatient.php"><button>Consult Now</button></a>
+                    </div>
+                </div>
+
+            </div>
+            <br>
+
+            <div style="text-align:center; display: none;">
+                <span class="dot"></span>
+                <span class="dot"></span>
+                <span class="dot"></span>
+            </div>
+
+        </div>
+    </section>
+    <section id="services" class="services">
+       <div class="container">
+           <h1 class="section-heading"><span>Why</span >Mandna</h1>
+           
+           <div class="card-wrapper">
+               <div class="card">
+                <img src="/images/newconsult.jpg" alt="">
+                    <h2>Doctors who care</h2>
+                <p>Our Doctors listen to you patiently and only prescribe what's necessary.</p>
+               </div>
+               <div class="card">
+                <img src="./images/brush.svg" alt="">
+                   <h2>Curated Doctors</h2>
+                <p>We hand-pick doctors for you , onboarded after a multistep screening process.</p>
+               </div>
+               <div class="card">
+                <img src="./images/brush.svg" alt="">
+                   <h2>Transparent & Secure</h2>
+                <p>Nothing is hidden Access your medical records in your health locker,24x7.</p>
+               </div>
+               
+           </div>
+       </div>
+   </section>
+    <section id="consult" class="virtual-consultation mobile-mode">
+        <div class="consult-container">
+            <div class="consult-header">
+                <h2>Schedule a Consultation</h2>
+            </div>
+            <div class="consult-content">
+                <div class="consult-right">
+                    <img src="images/consult-img.png" alt="Consult with a doctor">
+                </div>
+                <div class="consult-left">
+                    <div class="consult-info">
+                        <h4>Fill a form, fix a date and get your appointment</h4>
+                        <p>
+                            Are you facing a health condition, issue or challenge that require you seeing a specialist doctor? Well, we can help you schedule a meeting with any specialist doctor at your own comfortable date. Just make sure you pick a date at least three days ahead
+                            so the doctor can fix your appointment appropraitely.
+                        </p>
+                        <p>
+                            We currently have provisions for the following specialists: <br><br> - Psycologists&nbsp;&nbsp;&nbsp;- Obsidian&nbsp;&nbsp;&nbsp;- Physoitherapist <br> - Gyneolgists&nbsp;&nbsp;&nbsp;- Optamologist&nbsp;&nbsp;&nbsp;- Optician
+                            <br> - Dentist&nbsp;&nbsp;&nbsp;- Psychiatrist&nbsp;&nbsp;&nbsp;- Orthopedrist <br><br>Click the link below to fill the form.
+                        </p>
+                        <br>
+                        <a href="applicationlayer/login.php">Schedule an Appointment</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="consult1" class="virtual-consultation desktop-mode">
+        <div class="consult-container">
+            <div class="consult-header">
+                <h2>Schedule a Consultation</h2>
+            </div>
+            <div class="consult-content">
+                <div class="consult-left">
+                    <div class="consult-info">
+                        <h4>Fill a form, fix a date and get your appointment</h4>
+                        <p>
+                            Are you facing a health condition, issue or challenge that require you seeing a specialist doctor? Well, we can help you schedule a meeting with any specialist doctor at your own comfortable date. Just make sure you pick a date at least three days ahead
+                            so the doctor can fix your appointment appropraitely.
+                        </p>
+                        <p>
+                            We currently have provisions for the following specialists: <br><br> - Psycologists&nbsp;&nbsp;&nbsp;- Obsidian&nbsp;&nbsp;&nbsp;- Physoitherapist <br> - Gyneolgists&nbsp;&nbsp;&nbsp;- Optamologist&nbsp;&nbsp;&nbsp;- Optician
+                            <br> - Dentist&nbsp;&nbsp;&nbsp;- Psychiatrist&nbsp;&nbsp;&nbsp;- Orthopedrist <br><br>Click the link below to fill the form.
+                        </p>
+                        <br>
+                        <a href="applicationlayer/login.php">Schedule an Appointment</a>
+                    </div>
+                </div>
+                <div class="consult-right">
+                    <img src="images/consult-img.png" alt="Consult with a doctor">
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="pharm" class="virtual-pharmacy">
+        <div class="pharm-container">
+            <div class="pharm-header">
+                <h2>Pharmacy</h2>
+            </div>
+            <div class="pharm-content">
+                <div class="pharm-right">
+                    <img src="images/pharm-img.png" alt="Consult with a doctor">
+                </div>
+                <div class="pharm-left">
+                    <h4>We verify your prescriptions and provide you with the right pills</h4>
+                    <p>
+                        Whether you have consulted with a distant family doctor or you want us to prescribe drugs for you based on symptoms, we are here for you. You can verify the drugs with our pharmacist and also consult with him about your symptoms for the right drug prescription.
+                    </p>
+                    <p>NOTE: Consulting with our pharmacist is for minor symptoms. If symptoms persist after three days, see a doctor.</p>
+                    <div class="pharm-links">
+                        <button id="myBtn">Consult with our Pharmacist</button>
+
+                        <!-- The Modal -->
+                        <div id="myModal" class="modal">
+
+                            <!-- Modal content -->
+                            <div class="modal-content">
+                                <span class="close">&times;</span>
+                                <div data-tf-widget="TuwtiqnS" data-tf-opacity="100" data-tf-chat style="width:100%;height:100%;"></div>
+                                <script src="//embed.typeform.com/next/embed.js"></script>
+                            </div>
+
+                        </div>
+                    </div>
+                    <div class="pharm-links">
+                        <button id="myBtn1">Submit Subscription</button>
+
+                        <!-- The Modal -->
+                        <div id="myModal1" class="modal">
+
+                            <!-- Modal content -->
+                            <div class="modal-content">
+                                <span class="close">&times;</span>
+                                <div data-tf-widget="TuwtiqnS" data-tf-opacity="100" data-tf-chat style="width:100%;height:100%;"></div>
+                                <script src="//embed.typeform.com/next/embed.js"></script>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+  <!--Contact Ends Here-->
+    <footer>
+        <div class="foot">
+            <img src="images/Mandna-logos_white.png" alt="Mandna">
+            <p>&#169; Copyright 2022 Mandna</p>
+        </div>
+    </footer>
+    <script src="js/custom.js"></script>
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script>
+        function ham() {
+            $('.navicon').toggleClass('navicon--active');
+            $('.toggle').toggleClass('toggle--active');
+        }
+        alert("WELCOME TO OUR SITE!");
+    </script>
+    
+</body>
+
+</html>
